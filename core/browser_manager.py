fe200caabc1380a55ef5c511fa2ec103f8cb563c
wand3 <godswillbaiyewu@gmail.com>
2025-09-27 03:29:57 +0100

    fingerprinting improvements for context added

--- core/browser_manager.py
+++ core/browser_manager.py
@@ -2,7 +2,7 @@ from playwright.async_api import async_playwright, Browser, BrowserContext, Page
 from utils.fs_utils import ensure_parent_folder
 import os
 from utils.logger import get_logger
-
+import json
 
 class BrowserManager:
     # def __init__(self, headless: bool = False, storage_state_path: str = None):
@@ -15,10 +15,9 @@ class BrowserManager:
         # Create (if needed) a hidden folder one level above project root
         storage_dir = ensure_parent_folder(".storage_state")
         # Determine path for this context’s storage state JSON
+        self.state_filename = f"cookies.json"
         self.storage_state_path = (
-            storage_dir / state_filename
-            if state_filename
-            else None
+            storage_dir / self.state_filename
         )
         # Determine path for this context’s storage state JSON
         # self.storage_state_path = storage_dir
@@ -29,29 +28,74 @@ class BrowserManager:
 
     async def __aenter__(self) -> Page:
         self.playwright = await async_playwright().start()
-        self.browser = await self.playwright.firefox.launch(headless=self.headless)
+        self.browser = await self.playwright.firefox.launch(headless=self.headless,
+                                                            firefox_user_prefs={
+                                                                "media.peerconnection.enabled": False
+                                                            }
+                                                            )
         # If we have a saved state file, load it
         if self.storage_state_path and self.storage_state_path.is_file():
-            logger.info(f"Loading storage state from {self.storage_state_path}")
+            self.logger.info(f"Loading storage state from {self.storage_state_path}")
             self.context = await self.browser.new_context(
-                storage_state=str(self.storage_state_path)
+                storage_state=str(self.storage_state_path),
+                viewport={"width": 1440, "height": 900},  # macbook air viewport size
+                user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0",
+                locale="en-NG",  # 'en-NG' for English in Nigeria
+                timezone_id="Africa/Lagos",  # Lagos is the most common timezone for Nigeria, including Abuja
+                color_scheme="light",
+                permissions=[],
+                extra_http_headers={
+                    "Accept-Language": "en-NG,en;q=0.9",  # Prioritize Nigerian English
+                    "Sec-Fetch-Dest": "document",
+                    "Sec-Fetch-Mode": "navigate",
+                    # Consider adding a realistic User-Agent
+                    "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0"
+
+                }
             )
+            # with open(self.storage_state_path, "r") as f:
+            #     cookies = json.load(f)
+            #     await self.context.add_cookies(cookies)
+            #     self.logger.info(f"Cookies loaded for successfully")
         else:
-            if self.storage_state_path:
+            if not storage_dir:
+                ensure_parent_folder(str(self.storage_state_path))
                 logger.info(f"No existing state file at {self.storage_state_path}, starting fresh")
-            self.context = await self.browser.new_context()
+            self.context = await self.browser.new_context(
+                storage_state=None,
+                viewport={"width": 1440, "height": 900},  # macbook air viewport size
+                user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0",
+                locale="en-NG",  # 'en-NG' for English in Nigeria
+                timezone_id="Africa/Lagos",  # Lagos is the most common timezone for Nigeria, including Abuja
+                color_scheme="light",
+                permissions=[],
+                extra_http_headers={
+                    "Accept-Language": "en-NG,en;q=0.9",  # Prioritize Nigerian English
+                    "Sec-Fetch-Dest": "document",
+                    "Sec-Fetch-Mode": "navigate",
+                    # Consider adding a realistic User-Agent
+                    "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0"
+
+                }
+            )
+            # with open(self.storage_state_path, "r") as f:
+            #     cookies = json.load(f)
+            #     await context.add_cookies(cookies)
+            #     self.logger.info(f"Cookies loaded for successfully")
+        # Disable webdriver detection
+        await self.context.add_init_script(
+            """
+                delete navigator.__proto__.webdriver;
+                Object.defineProperty(navigator, 'plugins', {
+                    get: () => [1, 2, 3]
+                });
+            """
+        )
 
         page = await self.context.new_page()
+        page.set_default_timeout(15000)
+
         return page
-        # if self.storage_state_path and os.path.exists(self.storage_state_path):
-        #     self.context = await self.browser.new_context(
-        #         storage_state=self.storage_state_path
-        #     )
-        # else:
-        #     self.context = await self.browser.new_context()
-        #
-        # page = await self.context.new_page()
-        # return page
 
     async def __aexit__(self, exc_type, exc, tb) -> None:
         # Always save the updated storage state back to disk
--- strategies/insta_act.py
+++ strategies/insta_act.py
@@ -6,6 +6,7 @@ from utils.credentials import load_credentials
 from utils.logger import get_logger
 from utils.cookie_utils import save_cookies
 from pathlib import Path
+import asyncio
 
 short_delay = random.uniform(1, 4)
 
@@ -27,36 +28,81 @@ class InstaStrategy(InteractionStrategy):
         self.logger.info("InstaStrategy load success")
 
     async def _login(self, page: Page):
-        self.logger.info("Logging in to Insta")
-        # await page.wait_for_load_state()
-        time.sleep(short_delay)
+        self.logger.info("Navigating to Instagram login")        # await page.wait_for_load_state()
+        await asyncio.sleep(short_delay)
         try:
-            login = await page.query_selector("button[type='submit']:has-text('Log in')")
+            login = await page.wait_for_selector("button[type='submit']:has-text('Log in')")
             self.logger.info("Log in button spotted")
             if login:
                 await page.fill("input[name='username']", self.username)
-                time.sleep(short_delay)
-                await page.fill("input[name='password']", self.password)
-                time.sleep(short_delay)
-                await login.click(force=True)
+                await asyncio.sleep(short_delay)
+
+                password_field = page.get_by_label("Password")
+
+                passwordInputPos = await password_field.bounding_box()
+                self.logger.info(f"{passwordInputPos}")
+
+                await page.mouse.move(passwordInputPos["x"] + passwordInputPos["width"] / 2,
+                                      passwordInputPos["y"] + passwordInputPos["height"] / 2)
+                # await page.mouse.down()
+                await page.mouse.click(passwordInputPos["x"] + passwordInputPos["width"] / 2,
+                                       passwordInputPos["y"] + passwordInputPos["height"] / 2)
+                # await page.mouse.up()
+                await page.keyboard.type(f"{self.password}", delay=100)
+
+                # await page.fill("input[name='password']", self.password)
+                await asyncio.sleep(short_delay)
+                self.logger.info("Log in button click next")
+
+                await login.click()
+                self.logger.info("Log in button click success")
                 await page.wait_for_load_state()
+
+                await asyncio.sleep(short_delay)
+
         except Exception as e:
             self.logger.info(f"Login page error {e}")
-
-        # save login info
-        save_info = await page.query_selector("button[type='button']:has-text('Save info')")
-        if save_info:
-            await save_info.click(force=True)
-            save_cookies(page, self.storage_file)
-            self.logger.info("Save cookies success")
-
-        # await page.click("div[role='button']:has-text('Next')")
-        # await page.click("div[role='button']:has-text('Log in')")
-        # await page.wait_for_selector("nav[aria-label='Primary']")
-        # self.logger.info("Login successful")
+            raise e
 
     async def interact(self, page: Page):
         await page.goto("https://instagram.com/")
-        await page.wait_for_load_state()
-        await self._login(page)
+        # 1) If no cookie file, log in fresh and save cookies
+        # verify
+        value = page.evaluate("() => navigator.webdriver")
+        print("navigator.webdriver =", value)
+
+        # login = await page.query_selector("button[type='submit']:has-text('Log in')")
+        home = await page.query_selector("div[data-visualcompletion='ignore-dynamic']")
+
+        # if not self.storage_file.exists():
+        if not home:
+            await self._login(page)
+            await asyncio.sleep(short_delay)  # brief pause for stability
+            await page.wait_for_selector("div[data-visualcompletion='ignore-dynamic']")
+
+            try:
+                # save login info
+                save_info = await page.query_selector("button[type='button']:has-text('Save info')")
+                if save_info:
+                    self.logger.info('"Save info" button spotted')
+
+                    await save_info.click(force=True)
+                    self.logger.info('"Save info" button click success')
+
+                    await save_cookies(page, self.storage_file)
+                    self.logger.info("Save cookies success")
+            except Exception as e:
+                self.logger.info(f"Save cookies not successful: {e}")
+                raise e
+            await save_cookies(page, self.storage_file)
+            self.logger.info(f"Cookies saved to {self.storage_file}")
+        else:
+            # 2) Already logged in via storage state
+            self.logger.info("Using existing cookies")
+
+        # 3) Any further actions here…
+        await page.wait_for_selector("div[data-visualcompletion='ignore-dynamic']")
+        self.logger.info("InstaStrategy finished actions")
+        self.logger.info(f"la di la")
+        return
 
